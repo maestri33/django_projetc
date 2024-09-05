583e5b19b8e636689a703ca0d0ee567441b49b72
root <root@vmi2116975.contaboserver.net>
2024-09-05 20:47:04 -0300

    Commit inicial

---
+++ api-client/models/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CustomUser';
+export * from './JWT';
+export * from './Login';
+export * from './LoginResponse';
+export * from './OtpRequest';
+export * from './PasswordChange';
+export * from './PatchedCustomUser';
+export * from './Register';
+export * from './RestAuthDetail';
+export * from './TokenRefresh';
+export * from './TokenVerify';
+export * from './UserSignupStats';
---
+++ src/urls.py
@@ -0,0 +1,55 @@
+"""maestri-mvp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/stable/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.urls import path, include
+from django.views.generic import RedirectView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from apps.web.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap(),
+}
+
+urlpatterns = [
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
+    # redirect Django admin login to main login page
+    path("admin/login/", RedirectView.as_view(pattern_name="account_login")),
+    path("admin/", admin.site.urls),
+    path("dashboard/", include("apps.dashboard.urls")),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path("accounts/", include("allauth.urls")),
+    path("users/", include("apps.users.urls")),
+    path("", include("apps.web.urls")),
+    path("group-chat/", include("apps.group_chat.urls")),
+    path("support/", include("apps.support.urls")),
+    path("celery-progress/", include("celery_progress.urls")),
+    # auth API
+    path("api/auth/", include("apps.authentication.urls")),
+    # API docs
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI - you may wish to remove one of these depending on your preference
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    # hijack urls for impersonation
+    path("hijack/", include("hijack.urls", namespace="hijack")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.ENABLE_DEBUG_TOOLBAR:
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
